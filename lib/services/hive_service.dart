7af14ac1834b2d7371458c7fa355ede3c639404a
Jonny <adigunjohn5028@gmail.com>
2025-09-03 00:10:46 +0100

    final1 commit

--- lib/services/hive_service.dart
+++ lib/services/hive_service.dart
@@ -12,12 +12,12 @@ class HiveService{
     Hive.registerAdapter(SavedNewsAdapter());
     await Hive.openBox<SavedNews>(AppStrings.bookmarkedNews);
     await Hive.openBox<ThemeMode>(AppStrings.themeSettings);
-    await Hive.openBox<SavedNews>(AppStrings.offlineNews);
+    // await Hive.openBox<SavedNews>(AppStrings.offlineNews);
     log('hive successfully initialized');
   }
 
   static Box<SavedNews> savedBookmarkedNews = Hive.box(AppStrings.bookmarkedNews);
-  static Box<SavedNews> offlineSavedNews = Hive.box(AppStrings.offlineNews);
+  // static Box<SavedNews> offlineSavedNews = Hive.box(AppStrings.offlineNews);
   static Box<ThemeMode> themeSettingsStorage = Hive.box(AppStrings.themeSettings);
 
     ThemeMode? getThemeMode() {
@@ -38,14 +38,14 @@ class HiveService{
     }
   }
 
-  SavedNews? getOfflineNews(String key) {
-    try {
-      return offlineSavedNews.get(key);
-    } catch (e) {
-      log('Get Offline News storage failed: $e');
-      return null;
-    }
-  }
+  // SavedNews? getOfflineNews(String key) {
+  //   try {
+  //     return offlineSavedNews.get(key);
+  //   } catch (e) {
+  //     log('Get Offline News storage failed: $e');
+  //     return null;
+  //   }
+  // }
 
   void updateThemeMode({required ThemeMode theme}) {
       themeSettingsStorage.put(AppStrings.themeKey, theme);
@@ -57,10 +57,10 @@ class HiveService{
       log('Bookmarked News storage updated: $key -- $news');
   }
 
-  void updateOfflineNews({required String key, required SavedNews news}) {
-    offlineSavedNews.put(key, news);
-    log('Offline News storage updated: $key -- $news');
-  }
+  // void updateOfflineNews({required String key, required SavedNews news}) {
+  //   offlineSavedNews.put(key, news);
+  //   log('Offline News storage updated: $key -- $news');
+  // }
 
 
   void deleteBookmarkedNews({required String key}) {
@@ -68,10 +68,10 @@ class HiveService{
       log('Bookmarked News storage deleted: $key');
   }
 
-  void deleteOfflineNews({required String key}) {
-    offlineSavedNews.delete(key);
-    log('Offline News storage deleted: $key');
-  }
+  // void deleteOfflineNews({required String key}) {
+  //   offlineSavedNews.delete(key);
+  //   log('Offline News storage deleted: $key');
+  // }
 
   // Future<void> clearThemeSettingsStorage() async {
   //   await themeSettingsStorage.clear();
@@ -83,10 +83,10 @@ class HiveService{
     log('Bookmarked News storage has been cleared');
   }
 
-  Future<void> clearOfflineNews() async {
-    await offlineSavedNews.clear();
-    log('Offline News storage has been cleared');
-  }
+  // Future<void> clearOfflineNews() async {
+  //   await offlineSavedNews.clear();
+  //   log('Offline News storage has been cleared');
+  // }
 
   static Future<void> closeHive() async {
     await Hive.close();
